437bd0dfce992b1e41cb4290cdb12f75973141ab
Richalbert <richalbert@free.fr>
2019-04-24 16:16:09 +0200

    ajout d info de structure dans le fichier

--- example4.cpp
+++ example4.cpp
@@ -5,6 +5,17 @@
 #include <iostream>  
 #include <fstream>  
 
+struct bme280_data {
+        /*! Compensated pressure */
+        double pressure;
+        /*! Compensated temperature */
+        double temperature;
+        /*! Compensated humidity */
+        double humidity;
+};
+
+
+
 using namespace std;  
 
 int main () {  
@@ -13,10 +24,17 @@ int main () {
 	//It is used to create files and write information to the files.
     	ofstream filestreamOut("testout.txt");  
 
+
+	struct bme280_data bme = { 1020.6 , 27.7 , 45.3 }; 
+
     	if (filestreamOut.is_open())  
     	{  
         	filestreamOut << "Welcome to javaTpoint.\n";  
         	filestreamOut << "C++ Tutorial.\n";  
+        	filestreamOut << bme.pressure << " " 
+		              << bme.temperature << " "  
+			      << bme.humidity << " " << endl;  
+
         	filestreamOut.close();  
     	}  
     	else 
